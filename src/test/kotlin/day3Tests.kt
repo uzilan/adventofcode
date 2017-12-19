c0c60103cdaba1dc19478c61a3b461a87df06f7a
Uzi Landsmann <uzi.landsmann@gmail.com>
2017-12-19 17:25:03 +0100

    Added day 3, not really working though

---
+++ src/test/kotlin/day3Tests.kt
@@ -0,0 +1,27 @@
+import com.landsmann.adventofcode.checksum
+import com.landsmann.adventofcode.parseMatrix
+import com.landsmann.adventofcode.spiralMemory
+import com.landsmann.adventofcode.sumOfSimilar
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.forAll
+import io.kotlintest.properties.headers
+import io.kotlintest.properties.row
+import io.kotlintest.properties.table
+import io.kotlintest.specs.StringSpec
+
+class SpiralMemoryTests : StringSpec({
+
+    "Distance to spiral middle should result in correct value" {
+        val table = table(
+                headers("place in spiral", "expected result"),
+                //row(1, 0),
+                row(12, 3),
+                row(23, 2),
+                row(1024, 32)
+        )
+
+        forAll(table) { placeInSpiral, expectedResult ->
+            spiralMemory(placeInSpiral) shouldBe expectedResult
+        }
+    }
+})
