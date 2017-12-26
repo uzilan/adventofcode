7df010a9dd37c2896659a891aadf0e928281f2a0
Uzi Landsmann <uzi.landsmann@gmail.com>
2017-12-26 08:22:52 +0100

    Day 10, not working :(

--- src/main/kotlin/com/landsmann/adventofcode/day10.kt
+++ src/main/kotlin/com/landsmann/adventofcode/day10.kt
@@ -1,39 +1,47 @@
 package com.landsmann.adventofcode
 
 fun knotHash(input: List<Int>, list: List<Int> = (0..255).toList()): Int {
+    var currentPosition = 0
+    var skipSize = 0
+    var currentList = list
 
+    input.forEach {
+        currentList = reverseCirularRange(currentList, currentPosition, it)
+        currentPosition = it + skipSize
+        skipSize++
+    }
 
-    return 0
+    currentList = moveListXSteps(currentList, skipSize)
+
+    return currentList[0] * currentList[1]
 }
 
-// reverseCirularRange((0, 1, 2, 3, 4), x, y) -> move list x steps -> reverse y numbers -> move list -x steps
-// reverseCirularRange((0, 1, 2, 3, 4), 1, 0) -> (2, 3, 4, 0, 1) -> (2, 3, 4, 0, 1) ->  (0, 1, 2, 3, 4)
-// reverseCirularRange((0, 1, 2, 3, 4), 1, 1) -> (2, 3, 4, 0, 1) -> (3, 2, 4, 0, 1) ->  (0, 1, 3, 2, 4)
-// reverseCirularRange((0, 1, 2, 3, 4), 1, 2) -> () -> () -> ()
-// reverseCirularRange((0, 1, 2, 3, 4), 1, 3) -> (2, 3, 4, 0, 1) -> (0, 4, 3, 2, 1) -> (2, 1, 0, 4, 3)
 private fun reverseCirularRange(list: List<Int>, fromIndex: Int, numberOfElements: Int): List<Int> {
-    return emptyList()
+    val movedList = moveListXSteps(list, fromIndex)
+    val sublistToReverse = movedList.take(numberOfElements)
+    val reversedSublist = sublistToReverse.reversed()
+    val restOfList = movedList.takeLast(movedList.count() - numberOfElements)
+    val unionedList = reversedSublist.union(restOfList).toList()
+    val restoredList = moveListXStepsReverse(unionedList, fromIndex)
+
+    return restoredList
 }
 
-// moveListXSteps((0, 1, 2, 3, 4), 2) -> (2, 3, 4, 0, 1)
-private fun moveListXSteps(range: List<Int>, steps: Int): List<Int> {
-    val endWith = range.take(steps)
-    val beginWith = range.toList().subList(steps, range.count())
+private fun moveListXSteps(list: List<Int>, steps: Int): List<Int> {
+    val endWith = list.take(steps)
+    val beginWith = list.takeLast(list.count() - steps)
     val unioned = beginWith.union(endWith)
     return unioned.toList()
 }
 
-// moveListXStepsRevers((0, 1, 2, 3, 4), 2) -> (3, 4, 0, 1, 2)
-private fun moveListXStepsRevers(range: List<Int>, steps: Int): List<Int> {
-    val beginWith = range.toList().subList(range.count() - steps, range.count())
-    val endWith = range.take(range.count() - steps)
-    val unioned = beginWith.union(endWith)
-    return unioned.toList()
+private fun moveListXStepsReverse(list: List<Int>, steps: Int): List<Int> {
+    return moveListXSteps(list, list.count() - steps)
 }
 
 
 fun main(args: Array<String>) {
-    val shortRange = (0..4).toList()
-    val input = listOf(3, 4, 1, 5)
-    println(knotHash(input, shortRange))
+    //val shortRange = (0..4).toList()
+    //val input = listOf(3, 4, 1, 5)
+    val input = listOf(147, 37, 249, 1, 31, 2, 226, 0, 161, 71, 254, 243, 183, 255, 30, 70)
+    println(knotHash(input))
 }
